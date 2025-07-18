6bae63599c6b4d403bc63e0aeb4f7a98d5c3282c
Fly Dev <dev@fly.io>
2025-07-18 21:48:19 +0000

    add CSV import route to router

--- dras_mvp/lib/dras_mvp_web/router.ex
+++ dras_mvp/lib/dras_mvp_web/router.ex
@@ -18,6 +18,7 @@ defmodule DrasMvpWeb.Router do
     pipe_through :browser
 
     live "/", AcademicYearLive, :index
+    live "/csv-import", CsvImportLive
   end
 
   # Other scopes may use custom stacks.
